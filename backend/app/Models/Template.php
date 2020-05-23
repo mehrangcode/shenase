46e70dae0de486ce5e43dfa1d0281f0b0dc1a49d
Mehran <imganjgahi@gmail.com>
2020-05-24 03:48:07 +0430

    save template content as json

--- backend/app/Models/Template.php
+++ backend/app/Models/Template.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Template extends Model
 {
+    protected $casts = [
+        'content' => 'array'
+    ];
 
     protected $fillable = [
         'title',
